515c273f3c5773e022f85b692cc01584f41b2614
Loic Dal Zotto <loic.dalzotto@magicplan.app>
2024-08-05 15:14:15 +0200

    sq

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import { requireNativeComponent, ViewStyle, Platform } from "react-native";
 
 export type PanoramaViewProps = {
   imageUrl: string;
-  dimensions?: { width: number; height: number }; // Android-only
-  inputType?: "mono" | "stereo"; // Android-only
   enableTouchTracking?: boolean;
   onImageLoadingFailed?: () => void;
   onImageDownloaded?: () => void;
@@ -16,8 +14,6 @@ export const PanoramaView: React.FC<PanoramaViewProps> = ({
   onImageLoadingFailed,
   onImageDownloaded,
   onImageLoaded,
-  dimensions,
-  inputType,
   ...props
 }) => {
   const _onImageLoadingFailed = () => {
@@ -38,20 +34,9 @@ export const PanoramaView: React.FC<PanoramaViewProps> = ({
     }
   };
 
-  if (Platform.OS === "android" && !dimensions) {
-    console.warn('The "dimensions" property is required for PanoramaView on Android devices.');
-    return null;
-  }
-
-  if (Platform.OS === "ios" && inputType === "stereo") {
-    console.warn("The stereo inputType is currently only supported on Android devices.");
-  }
-
   return (
     <NativePanoramaView
       {...props}
-      dimensions={dimensions}
-      inputType={inputType}
       onImageDownloaded={_onImageDownloaded}
       onImageLoaded={_onImageLoaded}
       onImageLoadingFailed={_onImageLoadingFailed}
